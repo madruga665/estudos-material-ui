784692efb228e44460d4139a8d0b319dc10cb08b
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-02-25 19:00:33 -0300

    add rotas e homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
+import MyRoutes from "./routes";
 import { Theme } from "./styles/Theme";
 
 const App = () => {
   return (
     <ThemeProvider theme={Theme}>
+      <MyRoutes />
       <CssBaseline />
     </ThemeProvider>
   );
