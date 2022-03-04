f943cd463ca8087af31f78fe7a19df5f5935e525
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-03-04 10:24:26 -0300

    feat: add frase de email invalido no userSchema

--- src/validate/userSchema.ts
+++ src/validate/userSchema.ts
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 export const userSchema = yup.object({
   name: yup.string().required("Coloca o nome ai vagabundo"),
-  email: yup.string().email().required("Só de raiva vou encher teu email de spam"),
+  email: yup.string().email('Digita um email de verdade mano!').required("Só de raiva vou encher teu email de spam"),
   phone: yup.string().required("Eu juro que não vou te chamar no zap"),
   address: yup.string().required("Coloca o seu endereço ai vacilão"),
 });
