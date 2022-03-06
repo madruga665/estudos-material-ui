bd9c0e9fbb401c7edf3bb48baccbad8c70a8252a
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-03-06 11:55:10 -0300

    feat: add Componente Dropdown

--- src/pages/FormPage/index.tsx
+++ src/pages/FormPage/index.tsx
@@ -4,6 +4,7 @@ import Form from "../../components/Form";
 import Header from "../../components/Header";
 import { FormPageContainer } from "./styles";
 import Formik from "../../hooks/Formik";
+import Dropdown from "../../components/Dropdown";
 
 const FormPage = () => {
   const { formik } = Formik();
@@ -13,45 +14,46 @@ const FormPage = () => {
       <Header pageTitle='Formulário' />
       <Form handleSubmit={formik.handleSubmit}>
         <TextField
+          variant='standard'
           error={formik.errors.name && formik.touched.name ? true : false}
           helperText={formik.errors.name && formik.touched.name ? formik.errors.name : null}
           margin='normal'
           id='name'
           label='Nome'
-          variant='standard'
           value={formik.values.name}
           onChange={formik.handleChange}
         />
         <TextField
+          variant='standard'
           error={formik.errors.email && formik.touched.email ? true : false}
           helperText={formik.errors.email && formik.touched.email ? formik.errors.email : null}
           margin='normal'
           id='email'
           label='Email'
-          variant='standard'
           value={formik.values.email}
           onChange={formik.handleChange}
         />
         <TextField
+          variant='standard'
           error={formik.errors.phone && formik.touched.phone ? true : false}
           helperText={formik.errors.phone && formik.touched.phone ? formik.errors.phone : null}
           margin='normal'
           id='phone'
           label='Telefone'
-          variant='standard'
           value={formik.values.phone}
           onChange={formik.handleChange}
         />
         <TextField
+          variant='standard'
           error={formik.errors.address && formik.touched.address ? true : false}
           helperText={formik.errors.address && formik.touched.address ? formik.errors.address : null}
           margin='normal'
           id='address'
           label='Endereço'
-          variant='standard'
           value={formik.values.address}
           onChange={formik.handleChange}
         />
+        <Dropdown />
         <Button sx={{ marginTop: "5rem" }} type='submit' variant='contained'>
           Enviar
         </Button>
